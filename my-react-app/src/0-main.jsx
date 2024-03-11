c06abb01a2be3e0d6d7530ac9d9683dd8a81413a
Igor Beraldo <igorberaldomo@gmail.com>
2024-03-11 11:04:52 -0300

    changes

--- my-react-app/src/0-main.jsx
+++ my-react-app/src/0-main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './8-forms.jsx'
+import App from './0-app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
